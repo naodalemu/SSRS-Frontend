0ac916c4534ac9c91a4651c5f44721f0786ddac4
Naod Alemu <naodalemu11@gmail.com>
2025-02-09 02:42:29 +0300

    updated the sign up page. It is almost done

--- src/assets/components/SignUp.jsx
+++ src/assets/components/SignUp.jsx
@@ -1,24 +1,135 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-import '@splidejs/react-splide/css';
+import "@splidejs/react-splide/css";
 import classes from "./SignUp.module.css";
+import { Link } from "react-router-dom";
 
 function SignUp() {
   return (
     <section className={classes.signup}>
-      <div className="left-carousel" style={{ width: "48%", borderRadius: "10px", overflow: "hidden", }}>
-        <Splide aria-label="My Favorite Images" options={{rewind: true, gap: '1rem', autoplay: true, interval: 5000, arrows: false, type: "loop", heightRatio: 0.9, autoHeight: true, autoWidth: true, pagination: true,}} className={classes.splide} >
+      <div className={classes.leftCarousel}>
+        <Splide
+          aria-label="My Favorite Images"
+          options={{
+            rewind: true,
+            gap: "1rem",
+            autoplay: true,
+            interval: 5000,
+            arrows: false,
+            type: "loop",
+            heightRatio: 0.9,
+            autoHeight: true,
+            autoWidth: true,
+            pagination: true,
+          }}
+          className={classes.splide}
+        >
           <SplideSlide>
-            <img src="src/assets/images/buffalo_wings.jpg" alt="Image 1" className={classes.slideImage} />
+            <div className={classes.slideContent}>
+              <img
+                src="src/assets/images/Have Customized Order.jpg"
+                alt="Buffalo Wings"
+                className={classes.slideImage}
+              />
+              <div className={classes.textOverlay}>
+                <h2>
+                  Have a <br />
+                  Customized <br />
+                  Order Call
+                </h2>
+              </div>
+            </div>
           </SplideSlide>
           <SplideSlide>
-            <img src="src/assets/images/carrot_juice.jpg" alt="Image 2" className={classes.slideImage} />
+            <div className={classes.slideContent}>
+              <img
+                src="src/assets/images/Start Getting Your Order History.jpg"
+                alt="Carrot Juice"
+                className={classes.slideImage}
+              />
+              <div className={classes.textOverlay}>
+                <h2>
+                  Start Getting <br />
+                  Your Order <br />
+                  History
+                </h2>
+              </div>
+            </div>
           </SplideSlide>
           <SplideSlide>
-            <img src="src/assets/images/grilled_salmon.jpg" alt="Image 3" className={classes.slideImage} />
+            <div className={classes.slideContent}>
+              <img
+                src="src/assets/images/Alternate Black.jpg"
+                alt="Grilled Salmon"
+                className={classes.slideImage}
+              />
+              <div className={classes.textOverlay}>
+                <h2>
+                  Experience <br />
+                  Amazing <br />
+                  Food
+                </h2>
+              </div>
+            </div>
           </SplideSlide>
         </Splide>
       </div>
-      <div className="right-content"></div>
+      <div className={classes.rightContent}>
+        <div className={classes.headerContainer}>
+          <h1 className={classes.headerText}>Create an account</h1>
+          <p className={classes.loginOp}>
+            Already have an account?{" "}
+            <Link to="/login">
+              <span className={classes.loginLink}>Log in</span>
+            </Link>
+          </p>
+        </div>
+
+        <form className={classes.signupForm}>
+          <input
+            type="text"
+            placeholder="Full Name"
+            className={classes.inputField}
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            className={classes.inputField}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className={classes.inputField}
+          />
+          <input
+            type="password"
+            placeholder="Confitm Password"
+            className={classes.inputField}
+          />
+
+          <div className={classes.checkboxContainer}>
+            <input type="checkbox" id="terms" className={classes.checkbox} />
+            <label htmlFor="terms">
+              I agree to the{" "}
+              <Link to="/terms" className={classes.termsLink}>
+                Terms & Conditions
+              </Link>
+            </label>
+          </div>
+
+          <button type="submit" className={classes.signupButton}>
+            Create account
+          </button>
+
+          <div className={classes.orDivider}>
+            <span>Or register with</span>
+          </div>
+
+          <button className={classes.googleButton}>
+            <img src="src/assets/images/Google.png" alt="Google" />
+            Google
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
